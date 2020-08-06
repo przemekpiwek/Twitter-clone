4bcbb524ff15753a8135bf50f7ee008e1310487c
Przemek Piwek <przemekpiwek@gmail.com>
2020-08-06 11:11:56 -0600

    Added all routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Notifications from "./components/Notifications";
+import Bookmarks from "./components/Bookmarks";
+import TweetDetails from "./components/TweetDetails";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/notifications">
+          <Notifications />
+        </Route>
+        <Route path="/bookmarks">
+          <Bookmarks />
+        </Route>
+        <Route path="/tweet/:tweetId">
+          <TweetDetails />
+        </Route>
+        <Route path="/profileId">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
